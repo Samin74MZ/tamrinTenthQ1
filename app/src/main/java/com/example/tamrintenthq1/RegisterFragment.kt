65c77afcadcfc6746c6dc0bb10e73d6e733b121d
Samin <bmst555m@gmail.com>
2022-03-16 09:19:13 +0330

    add safe args

--- app/src/main/java/com/example/tamrintenthq1/RegisterFragment.kt
+++ app/src/main/java/com/example/tamrintenthq1/RegisterFragment.kt
@@ -1,14 +1,28 @@
 package com.example.tamrintenthq1
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.example.tamrintenthq1.databinding.FragmentRegisterBinding
 
+
+const val FullName="fullName"
+const val UserName="userName"
+const val Email="email"
+const val Password="password"
+const val Gender="gender"
 
 class RegisterFragment : Fragment() {
-
+    lateinit var binding:FragmentRegisterBinding
+    var listOfInfo= mutableListOf<EditText>()
+    var gender=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,8 +33,73 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding= FragmentRegisterBinding.inflate(layoutInflater,container,false)
+        sharedPreferences = this.activity?.getSharedPreferences("Info", Context.MODE_PRIVATE)
+        initView()
+        return binding.root
+    }
+    private fun initView() {
+        listOfInfo.add(binding.evFullName)
+        listOfInfo.add(binding.evUsername)
+        listOfInfo.add(binding.evEmail)
+        listOfInfo.add(binding.evPassword)
+        listOfInfo.add(binding.evReTypePassword)
+        if (sharedPreferences?.getString(FullName,"")!="") {
+            setInfo()
+            register()
+        }else {
+            register()
+        }
     }
 
+    private fun setInfo() {
+        binding.evFullName.setText(sharedPreferences?.getString(FullName,""))
+        binding.evUsername.setText(sharedPreferences?.getString(UserName,""))
+        binding.evEmail.setText(sharedPreferences?.getString(Email,""))
+        binding.evPassword.setText(sharedPreferences?.getString(Password,""))
+        binding.evReTypePassword.setText(sharedPreferences?.getString(Password,""))
+        when(sharedPreferences?.getString(Gender,"")){
+            "Female"->binding.radioBtnFemale.isChecked=true
+            "Male"->binding.radioBtnMale.isChecked=true
+        }
+    }
+
+    private fun register() {
+        binding.btnRegister.setOnClickListener {
+            if(isCheck()){
+               var fullName=binding.evFullName.text.toString()
+                var userName=binding.evUsername.text.toString()
+                var email=binding.evEmail.text.toString()
+                var password=binding.evPassword.text.toString()
+                var gender=gender
+                val action=RegisterFragmentDirections.actionRegisterFragmentToShowInfoFragment(fullName,userName,email,password,gender)
+                findNavController().navigate(action)
+            }
+        }
+    }
+
+    private fun isCheck():Boolean{
+        var result=true
+        when {
+            binding.radioBtnFemale.isChecked -> gender="Female"
+            binding.radioBtnMale.isChecked -> gender="Male"
+            else -> {
+                Toast.makeText(context, "Enter Gender!", Toast.LENGTH_SHORT).show()
+                result=false
+            }
+        }
+        listOfInfo.forEach{
+            if(it.text.isBlank()) {
+                it.error ="Can not be empty!"
+                result= false
+            }
+        }
+
+        if(binding.evPassword.text.toString()!= binding.evReTypePassword.text.toString()){
+            binding.evReTypePassword.error="Password is not match!"
+            result=false
+        }
+        return result
+    }
 
 }
--- app/src/main/java/com/example/tamrintenthq1/ShowInfoFragment.kt
+++ app/src/main/java/com/example/tamrintenthq1/ShowInfoFragment.kt
@@ -1,13 +1,22 @@
 package com.example.tamrintenthq1
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.tamrintenthq1.databinding.FragmentShowInfoBinding
 
+var sharedPreferences: SharedPreferences?=null
 
 class ShowInfoFragment : Fragment() {
+    lateinit var binding: FragmentShowInfoBinding
+
+   val args:ShowInfoFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +28,31 @@ class ShowInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_info, container, false)
+        binding= FragmentShowInfoBinding.inflate(layoutInflater,container,false)
+        sharedPreferences= this.activity?.getSharedPreferences("Info", Context.MODE_PRIVATE)
+        getInfo()
+        saveInfo()
+        return binding.root
     }
+    private fun getInfo(){
+        binding.tvFullName.text=args.fullname
+        binding.tvUsername.text=args.username
+        binding.tvEmail.text=args.email
+        binding.tvPassword.text=args.password
+        binding.tvGenderSf.text=args.gender
 
+    }
+    private fun saveInfo(){
+        val editor= sharedPreferences?.edit()
+        binding.btnSave.setOnClickListener {
+            editor?.putString(FullName, binding.tvFullName.text.toString())
+            editor?.putString(UserName, binding.tvUsername.text.toString())
+            editor?.putString(Email, binding.tvEmail.text.toString())
+            editor?.putString(Password, binding.tvPassword.text.toString())
+            editor?.putString(Gender, binding.tvGenderSf.text.toString())
+            editor?.apply()
+            findNavController().navigate(R.id.action_showInfoFragment_to_registerFragment)
+        }
+    }
 
 }
